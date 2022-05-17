876097c842a0f6e7972aeb6f940a1a7d04f94cd0
Sos Avetyan <avetyansos@gmail.com>
2022-05-17 14:19:06 +0400

    Initial Commit

---
+++ TestFlatRockTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFlatRockTech
+//
+//  Created by Sos Avetyan on 5/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
